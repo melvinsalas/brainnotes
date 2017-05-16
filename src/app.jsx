0bc2956e6d73599563030708118c5d60c88d7bda
Melvin Salas <msalas@coinca.tv>
2017-05-16 14:38:34 -0600

    reference bootstrap directly

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import '../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 import './scss/app.scss';
 
 const App = () => (
-  <div>
+  <div className="container">
     <h1>It's alive!</h1>
   </div>
 );
